92087b2b1e969780fa898069330c37dc01be549e
Vladislav <vlad.tyrinov.00@list.ru>
2023-12-21 16:45:33 +0300

    Add profect

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,44 @@
+import project01 from "./../img/projects/project-1/01.jpg";
+import project01Big from "./../img/projects/project-1/01-big.jpg";
+
+import project02 from "./../img/projects/project-2/02.jpg";
+import project02Big from "./../img/projects/project-2/02-big.jpg";
+
+import project03 from "./../img/projects/project-3/03.jpg";
+import project03Big from "./../img/projects/project-3/03-big.jpg";
+
+import project04 from "./../img/projects/project-4/04.jpg";
+import project04Big from "./../img/projects/project-4/04-big.jpg";
+
+const projects = [
+    {
+        title: 'Sanatorium',
+        skills: 'HTML, CSS, JavaScript',
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: 'https://github.com/tyrinvs/hotel-special-offer',
+    },
+    {
+        title: 'GoTrip',
+        skills: 'HTML, CSS, JavaScript',
+        img: project02,
+        imgBig: project02Big,
+        gitHubLink: 'https://github.com/tyrinvs/GoTrip',
+    },
+    {
+        title: 'Odigo',
+        skills: 'HTML, CSS, JavaScript',
+        img: project03,
+        imgBig: project03Big,
+        gitHubLink: 'https://github.com/tyrinvs/Odigo',
+    },
+    {
+        title: 'Recipes',
+        skills: 'HTML, CSS, JavaScript',
+        img: project04,
+        imgBig: project04Big,
+        gitHubLink: 'https://github.com/tyrinvs/Recipes',
+    }
+];
+
+export {projects}
